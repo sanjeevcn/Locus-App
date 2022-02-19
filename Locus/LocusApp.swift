75724a95e8bd9fd9950a497d3a73639c750019b3
Sanjeev <sanjeevcn@outlook.com>
2022-02-20 01:43:55 +0530

    Initial Commit

---
+++ Locus/LocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocusApp.swift
+//  Locus
+//
+//  Created by Sanjeev on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct LocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
